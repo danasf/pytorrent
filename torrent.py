e6c7b6a7b68a8aed9f110f22cc1b8a38e54e8acf
Dana <dsniezko@gmail.com>
2014-07-08 16:32:07 -0400

    connects to peers, sends str

--- torrent.py
+++ torrent.py
@@ -1,22 +1,26 @@
 import requests
 import hashlib
 import bencode
-import sys
+import socket
 from struct import *
 
-class Torrent(object):
+
+class Tracker(object):
 
 	def __init__(self):
 		print "Initialized"
 
 		self.DEBUG = False
+		self.tracker_params = ''
 		self.peer_id = '-ZZ0001-123456789012'
 		self.port = 6889
 		self.infohash = ''
 		self.file_len = 0
 		self.peers = []
 
+
 	def readTorrent(self,myFile):
+		'''Reads the torrent file, decodes, returns tracker announce'''
 		print "Reading torrent..."
 		data = bencode.bdecode(myFile)
 		self.infohash = hashlib.sha1(bencode.bencode(data['info'])).digest()
@@ -39,27 +43,28 @@ class Torrent(object):
 		if self.DEBUG:
 			print data
 		
-		tracker_params = {'peer_id':self.peer_id  , 'port':self.port ,'info_hash': self.infohash, 'left':self.file_len, 'uploaded': 0, 'downloaded': 0, 'numwant':5, 'supportcrypto':0,'compact':1, 'event':'started' }
-		return self.getPeers(data['announce'],tracker_params)
+		self.tracker_params = {'peer_id':self.peer_id  , 'port':self.port ,'info_hash': self.infohash, 'left':self.file_len, 'uploaded': 0, 'downloaded': 0, 'numwant':15, 'supportcrypto':0,'compact':1, 'event':'started' }
+		return data['announce']
 
-	def getPeers(self,url,params):
-		print "Sending reqest to tracker"
+	def getPeers(self,url):
+	 	'''Sends request to tracker, returns peers bytestring'''
 		print url
-		r = requests.get(url, params=params)
+		r = requests.get(url, params=self.tracker_params)
 		res = r.text
 		res = bencode.bdecode(res)
 		if self.DEBUG:
 			print r.status_code
 			print r.headers
 			print res
+		return res['peers']
 
-		return self.parsePeers(res['peers'])
-
+	# every 6 bytes is a new peer
 	# first 4 bytes are the ip address, 
 	# last 2 are port number char*256+char	
 	def parsePeers(self,peer_list):
+		'''Parses peers bytestring, returns list of peers'''
 		for i,char in enumerate(peer_list):
-			# when pos == 0 reset
+			# when a new peer reset
 			if i%6 == 0:
 				peer_ip = ""
 				peer_port = ""
@@ -69,14 +74,66 @@ class Torrent(object):
 			# when post == 4 start recording the port and save the peer_ip
 			elif i%6 == 4:
 				peer_port=ord(char)*256
-
 			elif i%6 == 5:
 				peer_port+=ord(char)
-				#print peer_port
 				self.peers.append([peer_ip[:-1],peer_port])
-
 		return self.peers
 
-tor = Torrent()
+class HandShake(object):
+
+	def __init__(self,infohash):
+		self.pstrlen = chr(19)
+		self.pstr = "BitTorrent protocol"
+		self.reserved = "\x00\x00\x00\x00\x00\x00\x00\x00"
+		self.info_hash = infohash
+		self.peer_id = "-ZZ0001-123456789012"
+
+
+	def makeStr(self):
+		return self.pstrlen+self.pstr+self.reserved+self.info_hash+self.peer_id
+
+class Message(object):
+
+	def __init__(self,data):
+		self.types = { 0:'choke',
+					   1:'unchoke',
+					   2:'interested',
+					   3:'not interested',
+					   4:'have',
+					   5:'bitfield',
+					   6:'request'
+					   }
+		self.data = data
+
+	def parse(self):
+		print self.data
+		if len(self.data) > 68:
+			ints = map(ord,self.data[69:])
+			print ints
+
+	def send(self,message):
+		pass
+
+
+tor = Tracker()
 myFile = open("ubuntu.iso.torrent","rb").read()
-print tor.readTorrent(myFile)
+loc = tor.readTorrent(myFile)
+peers = tor.parsePeers(tor.getPeers(loc))
+
+hs = HandShake(tor.infohash)
+hs_str = hs.makeStr()
+
+for i,peer in enumerate(peers):
+	print peer[0], peer[1]
+	try:
+		s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		s.connect((peer[0],peer[1]))
+		s.send(hs_str)
+		msg = Message(s.recv(2048))
+		msg.parse()
+		#s.write()
+		s.close()
+		# if data is longer than 68 chars there's a message
+		#print "from peer:", data
+	except socket.error:
+		print socket.error
